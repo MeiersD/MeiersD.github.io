20cb54383d196cea9572d08afa9db06258188872
MeiersD <meiersdixon@gmail.com>
2024-09-17 13:37:58 -0400

    moved loading bar to a seperate file for better reading

--- scripts/3d_rendering/3d.js
+++ scripts/3d_rendering/3d.js
@@ -1,11 +1,8 @@
 class test3d {
     constructor(file){
         this.scene_container = document.getElementById("scene-container");
-        this.loading_bar_container = document.getElementById('loading-bar-container');
-        this.loading_bar = document.getElementById("loading-bar");
-        this.progress = 0;
-        this.total_steps = 0;
         this.parse_pdb = new parsePDB(file, this);
+        this.loading_bar = new LoadingBar();
         this.atom_array = [];
         this.renderer = new THREE.WebGLRenderer();
 
@@ -14,7 +11,7 @@ class test3d {
     async mainSequence(){
         this.initialize_scene();
         this.parse_pdb.get_atom_lines().then(total_steps => {
-            this.show_loading_bar(total_steps);
+            this.loading_bar.show_loading_bar(total_steps);
             return new Promise(resolve => setTimeout(resolve, 100));
         }).then(() => {
             return this.parse_pdb.start_parsing();
@@ -45,7 +42,7 @@ class test3d {
             var atom = new THREE.Mesh(geometry, material);
             atom.position.set(this.atom_array[i].x_coord, this.atom_array[i].y_coord, this.atom_array[i].z_coord);
             protein.add(atom);
-            this.update_loading_bar(); // UPDATE LOADING BAR
+            this.loading_bar.update_loading_bar(); // UPDATE LOADING BAR
         }
         this.scene.add(protein);
 
@@ -56,42 +53,5 @@ class test3d {
             this.renderer.render(this.scene, this.camera);
         }
         animate();
-    }
-
-    show_loading_bar(total_steps) {    
-        // Make the loading bar visible
-        //total steps is the number of atoms in the pdb file.
-        this.loading_bar_container.classList.add('active');
-        this.loading_bar.classList.add('active');
-        this.total_steps = total_steps;
-        console.log("added loading bar, it ought to be active now");
-    }
-
-    async update_loading_bar() {   
-        await new Promise(resolve => setTimeout(resolve, 1000)); 
-        // Increase the progress based on the passed increment value
-        this.progress += 1;
-        // Ensure progress does not exceed total_steps
-        if (this.progress > this.total_steps) {
-            this.progress = this.total_steps;
-        }
-       
-        // Update the width of the loading bar
-        const progressPercentage = (this.progress / this.total_steps) * 100; 
-        //this.total_steps is of type promise. I need it to be an int
-        this.loading_bar.style.width = `${progressPercentage}%`;
-        console.log("bar updated, it is now", this.progress, "out of", this.total_steps);
-        // Check if loading is complete
-        if (this.progress === this.total_steps) {
-            setTimeout(() => {
-                this.hideLoadingBar();
-            }, 500);
-        }
-    }
-    
-    hideLoadingBar() {
-        this.loading_bar_container.classList.remove('active');
-        this.loading_bar.classList.remove('active');
-        this.loading_bar.style.width = '0%'; // Reset width for future uses
     }
 }
---
+++ scripts/3d_rendering/loading_bar.js
@@ -0,0 +1,47 @@
+class LoadingBar {
+    constructor(){
+        this.loading_bar_container = document.getElementById('loading-bar-container');
+        this.loading_bar = document.getElementById("loading-bar");
+        this.progress = 0;
+        this.total_steps = 0;
+        return;
+    }
+
+    show_loading_bar(total_steps) {    
+        // Make the loading bar visible
+        //total steps is the number of atoms in the pdb file.
+        this.loading_bar_container.classList.add('active');
+        this.loading_bar.classList.add('active');
+        this.total_steps = total_steps;
+        console.log("added loading bar, it ought to be active now");
+    }
+
+    async update_loading_bar() {   
+        await new Promise(resolve => setTimeout(resolve, 1000)); 
+        // Increase the progress based on the passed increment value
+        this.progress += 1;
+        // Ensure progress does not exceed total_steps
+        if (this.progress > this.total_steps) {
+            this.progress = this.total_steps;
+        }
+       
+        // Update the width of the loading bar
+        const progressPercentage = (this.progress / this.total_steps) * 100; 
+        //this.total_steps is of type promise. I need it to be an int
+        this.loading_bar.style.width = `${progressPercentage}%`;
+        console.log("bar updated, it is now", this.progress, "out of", this.total_steps);
+        // Check if loading is complete
+        if (this.progress === this.total_steps) {
+            setTimeout(() => {
+                this.hideLoadingBar();
+            }, 500);
+        }
+    }
+
+    hideLoadingBar() {
+        this.loading_bar_container.classList.remove('active');
+        this.loading_bar.classList.remove('active');
+        this.loading_bar.style.width = '0%'; // Reset width for future uses
+    }
+
+}
