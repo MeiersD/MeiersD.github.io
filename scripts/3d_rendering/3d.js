e1a42a5f04372818a0334a93074438d343d527c2
MeiersD <meiersdixon@gmail.com>
2024-09-25 10:36:04 -0400

    ar button console log

--- scripts/3d_rendering/3d.js
+++ scripts/3d_rendering/3d.js
@@ -58,6 +58,7 @@ class test3d {
         const ar_button = ARButton.createButton(renderer);
         scene_container.appendChild(ar_button);
         scene_container.classList.add("active");
+        console.log(ar_button);
     }
 
     set_up_scene() {
