dd72685d51ce4079637b311a98287224c85e94e2
MeiersD <meiersdixon@gmail.com>
2024-11-22 10:57:40 -0500

    added javadocs

--- scripts/front_end/pdb_fetcher.js
+++ scripts/front_end/pdb_fetcher.js
@@ -9,6 +9,12 @@ class PDBfetcher {
         this.keyword = '';
     }
 
+    /**
+     * adds event listener to the html text entry field. If enter is pressed,
+     * it will call the send_message_to_build_scene method with the current
+     *
+     * @param {*} pdb_input the html text entry field that contains the pdb code.
+     */
     check_pdb_input(pdb_input) {
         pdb_input.addEventListener('keydown', async (event) => {
             if (event.key === "Enter" && this.not_already_fetching) {
@@ -17,6 +23,9 @@ class PDBfetcher {
         });
     }
 
+    /**
+     * Fetches the pdb contents from RCSB using the provided pdb code and stores them in this.pdb_contents.
+     */
     async fetch_pdb_from_rcsb() {
         const response = await fetch(`https://files.rcsb.org/view/${this.pdb_code}.pdb`);
         if (!response.ok) {
@@ -25,6 +34,11 @@ class PDBfetcher {
         this.pdb_contents = await response.text();
     }
 
+    /**
+     * Gets the keyword from the pdb file by searching for the KEYWDS line
+     *
+     * @param {*} file The pdb file as a string
+     */
     get_keyword(file) {
         const lines = file.split('\n');
         const keyword_line = lines.filter(line => {
@@ -39,6 +53,11 @@ class PDBfetcher {
             .join(' ');
     }
 
+    /**
+     * Sends message to build scene when enter key is pressed on the text box
+     * 
+     * @param {*} pdb_input The pdb code entered into the text box
+     */
     async send_message_to_build_scene(pdb_input){
         this.pdb_code = pdb_input;
         console.log("You have inputted the code: ", this.pdb_code, "\nBeginning fetching now...");
@@ -47,10 +66,10 @@ class PDBfetcher {
         
         try {
             await this.fetch_pdb_from_rcsb(); // Wait for fetch to complete
-            this.home.request_approved_element(this.pdb_contents); 
+            this.home.showNotification("PDB found", "approved"); 
         } catch (error) {
             console.error("Error fetching PDB:", error);
-            this.home.request_denied_element(); // Handle fetch error
+            this.home.showNotification("PDB not found", "denied"); // Handle fetch error
             this.not_already_fetching = true; // Re-enable fetching
             return;
         }
@@ -71,16 +90,30 @@ class PDBfetcher {
         });
     }
 
+    /**
+     * passes this function directly to 3d.js
+     * @param {*} x_speed 
+     * @param {*} y_speed 
+     * @param {*} z_speed 
+     */
     change_rotation_speed(x_speed, y_speed, z_speed){
         this.temp.change_rotation_speed(x_speed, y_speed, z_speed);
     }
 
+    /**
+     * passes this function directly to 3d.js
+     * @param {*} value the trace status chosen by the user (atomic vs sticks)
+     */
     set_trace_status(value){
         if(this.temp){
             this.temp.set_trace_status(value);
         }
     }
 
+    /**
+     * passes this function directly to 3d.js
+     * @param {*} value the color chosen by the user
+     */
     change_coloring(value){
         if(this.temp){
             this.temp.change_coloring(value);
