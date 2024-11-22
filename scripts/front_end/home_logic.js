200f08d05d30cd136fe513c275141398e5cf3e7e
MeiersD <meiersdixon@gmail.com>
2024-11-22 10:50:47 -0500

    Added javadocs

--- scripts/front_end/home_logic.js
+++ scripts/front_end/home_logic.js
@@ -1,9 +1,19 @@
 class homeLogic {
+    /**
+     * Initializes the class by setting up references to the notification and input container elements.
+     */
     constructor(){
         this.notificationElement = document.getElementById("notification");
         this.input_container = document.getElementById("input-container");
         return;
     }
+
+    /**
+     * Shows a notification with the specified message and type.
+     *
+     * @param {string} message - The message to display in the notification.
+     * @param {string} type - The type of notification (e.g., "approved", "denied").
+     */
     showNotification(message, type) {
         this.notificationElement.innerText = message;
         this.notificationElement.classList.remove('approved', 'denied', 'show');
@@ -11,15 +21,6 @@ class homeLogic {
         setTimeout(() => {
             this.notificationElement.classList.remove('approved', 'denied', 'show');
         }, 3000); // Hide the notification after 3 seconds
-    }
-    request_approved_element(pdb_contents) {
-        console.log("pdb found");
-        this.showNotification("PDB found", "approved");
-    }
-
-    request_denied_element(pdb_contents) {
-        console.log("pdb not found");
-        this.showNotification("PDB not found", "denied");
     }
 }
 
